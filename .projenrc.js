b4635317a43b45aa6e0a679f8744055324871515
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-01-06 13:12:02 +0100

    chore: update dependencies

--- .projenrc.js
+++ .projenrc.js
@@ -11,9 +11,10 @@ const project = new CDKTFConstruct({
   devDeps: [
     '@cdktf/provider-null@0.4.90',
     '@cdktf/provider-random@0.0.5',
-    '@dschmidt/cdktf-construct-base@0.0.1',
+    '@dschmidt/cdktf-construct-base@0.0.5',
   ],
-  description: 'A simple construct that executes a command locally. This is useful to run build steps within your CDKTF Program or to run a post action after a resource is created.' /* The description is just a string that helps people understand the purpose of the package. */,
+  description:
+    'A simple construct that executes a command locally. This is useful to run build steps within your CDKTF Program or to run a post action after a resource is created.' /* The description is just a string that helps people understand the purpose of the package. */,
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
   // release: undefined,      /* Add release management to this project. */
