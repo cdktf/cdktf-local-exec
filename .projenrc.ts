ff0207070a023fde83ac7350bf7c49ac71496b3c
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-03-16 13:38:59 +0100

    chore: switch to MPL-2.0 license
    Not high-priority, but let’s be consistent with HashiCorp’s OSS practices and use the same license we use everywhere else.

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,13 +4,33 @@
  */
 
 import { CDKTFConstruct } from "@dschmidt/cdktf-construct-base";
+import { IResolver, License } from "projen";
+import { TypeScriptProject } from "projen/lib/typescript";
+
+const SPDX = "MPL-2.0";
+
+class CustomizedLicense extends License {
+  constructor(project: TypeScriptProject) {
+    super(project, { spdx: SPDX });
+
+    project.addFields({ license: SPDX });
+  }
+
+  synthesizeContent(resolver: IResolver) {
+    return (
+      "Copyright (c) 2022 HashiCorp, Inc.\n\n" +
+      super.synthesizeContent(resolver)
+    );
+  }
+}
 
 const project = new CDKTFConstruct({
-  author: "Daniel Schmidt",
-  authorAddress: "danielmschmidt92@gmail.com",
+  author: "HashiCorp",
+  authorAddress: "https://hashicorp.com",
+  authorOrganization: true,
   defaultReleaseBranch: "main",
   name: "cdktf-local-exec",
-  repositoryUrl: "https://github.com/DanielMSchmidt/cdktf-local-exec.git",
+  repositoryUrl: "https://github.com/cdktf/cdktf-local-exec.git",
   devDeps: ["@cdktf/provider-random", "@dschmidt/cdktf-construct-base"],
   description:
     "A simple construct that executes a command locally. This is useful to run build steps within your CDKTF Program or to run a post action after a resource is created." /* The description is just a string that helps people understand the purpose of the package. */,
@@ -21,7 +41,10 @@ const project = new CDKTFConstruct({
     name: "team-tf-cdk",
     email: "github-team-tf-cdk@hashicorp.com",
   },
+  licensed: false,
 });
+
+new CustomizedLicense(project);
 
 project.addPeerDeps("cdktf@>=0.15.0", "@cdktf/provider-null@>=5.0.0");
 
