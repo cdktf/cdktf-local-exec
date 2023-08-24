5ea0a61652c7a911218d6d5da25454b4566eac38
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2023-08-24 18:03:36 +0200

    chore(ci): remove Mergify (#166)
    Replace it with custom auto-approve and automerge workflows. Also
    updates all of our GitHub Actions to using pinned SHA hashes.

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,54 +3,43 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { IResolver, License } from "projen";
 import { ConstructLibraryCdktf } from "projen/lib/cdktf";
 import { UpgradeDependenciesSchedule } from "projen/lib/javascript";
-import { TypeScriptProject } from "projen/lib/typescript";
+import { AutoApprove } from "./projenrc/auto-approve";
+import { Automerge } from "./projenrc/automerge";
+import { CustomizedLicense } from "./projenrc/customized-license";
 
-const SPDX = "MPL-2.0";
+const name = "cdktf-local-exec";
 
-class CustomizedLicense extends License {
-  constructor(project: TypeScriptProject) {
-    super(project, { spdx: SPDX });
-
-    project.addFields({ license: SPDX });
-  }
-
-  synthesizeContent(resolver: IResolver) {
-    return (
-      "Copyright (c) 2021 HashiCorp, Inc.\n\n" +
-      super.synthesizeContent(resolver)
-    );
-  }
-}
+const githubActionPinnedVersions = {
+  "actions/checkout": "c85c95e3d7251135ab7dc9ce3241c5835cc595a9", // v3.5.3
+  "actions/download-artifact": "9bc31d5ccc31df68ecc42ccf4149144866c47d8a", // v3.0.2
+  "actions/setup-node": "64ed1c7eab4cce3362f8c340dee64e5eaeef8f7c", // v3.6.0
+  "actions/upload-artifact": "0b7f8abb1508181956e8e162db84b466c27e18ce", // v3.1.2
+  "amannn/action-semantic-pull-request":
+    "c3cd5d1ea3580753008872425915e343e351ab54", // v5.2.0
+  "peter-evans/create-pull-request": "284f54f989303d2699d373481a0cfa13ad5a6666", // v5.0.1
+};
 
 const project = new ConstructLibraryCdktf({
   author: "HashiCorp",
   authorAddress: "https://hashicorp.com",
   authorOrganization: true,
   defaultReleaseBranch: "main",
-  name: "cdktf-local-exec",
+  name,
   repositoryUrl: "https://github.com/cdktf/cdktf-local-exec.git",
   devDeps: ["@cdktf/provider-random", "ts-node@10.9.1"],
   description:
     "A simple construct that executes a command locally. This is useful to run build steps within your CDKTF Program or to run a post action after a resource is created." /* The description is just a string that helps people understand the purpose of the package. */,
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-  // release: undefined,      /* Add release management to this project. */
   workflowGitIdentity: {
     name: "team-tf-cdk",
     email: "github-team-tf-cdk@hashicorp.com",
   },
   cdktfVersion: "0.17.0",
-  autoApproveUpgrades: true,
-  autoApproveOptions: {
-    allowedUsernames: ["team-tf-cdk"],
-    label: "auto-approve",
-  },
+  mergify: false,
   depsUpgradeOptions: {
     workflowOptions: {
-      labels: ["auto-approve", "dependencies"],
+      labels: ["auto-approve", "automerge", "dependencies"],
       schedule: UpgradeDependenciesSchedule.WEEKLY,
     },
   },
@@ -58,19 +47,21 @@ const project = new ConstructLibraryCdktf({
   prettier: true,
   jsiiVersion: "5.0.7",
   publishToPypi: {
-    distName: "cdktf-local-exec",
-    module: "cdktf_local_exec",
+    distName: name,
+    module: name.replace(/-/g, "_"),
   },
   licensed: false,
 });
+
+new CustomizedLicense(project);
+new AutoApprove(project);
+new Automerge(project);
 
 project.addPeerDeps(
   "cdktf@>=0.17.0",
   "@cdktf/provider-null@>=7.0.0",
   "constructs@^10.0.25"
 );
-
-new CustomizedLicense(project);
 
 // Run copywrite tool to add copyright headers to all files
 project.buildWorkflow?.addPostBuildSteps(
@@ -80,5 +71,10 @@ project.buildWorkflow?.addPostBuildSteps(
   },
   { name: "Add headers using Copywrite tool", run: "copywrite headers" }
 );
+
+// Use pinned versions of github actions
+Object.entries(githubActionPinnedVersions).forEach(([action, sha]) => {
+  project.github?.actions.set(action, `${action}@${sha}`);
+});
 
 project.synth();
